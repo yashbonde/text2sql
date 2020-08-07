3f79eddcfb5ab1ecdc60b03671ed87ce3847fb88
yashbonde <bonde.yash97@gmail.com>
2020-08-07 15:57:27 +0530

    start trainer file + cool header

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
+<img src="assets/header.png">
+
 # Text2SQL using pretrained models
 
 ## Parsing
 
 In order to convert JSON objects into langauge models use the [`parse_to_lm.py`](./parse_to_lm.py), usage:
 ```
-➜  text2sql python3 parse_to_lm.py --help
-usage: parse_to_lm.py [-h] [--pairs PAIRS] [--tables TABLES]
-                      [--fresh-tokenizer [FRESH_TOKENIZER]]
+➜  text2sql git:(master) ✗ python3 parse_to_lm.py --help
 
 This file converts the dataset sentences to my format to be used for langauge
 modelling and use GPT insted of BERT models.
@@ -15,12 +15,68 @@ optional arguments:
   -h, --help            show this help message and exit
   --pairs PAIRS         path to pairs dump
   --tables TABLES       path to tables lm dump
+  --dev-pairs DEV_PAIRS
+                        path to dev pairs dump
   --fresh-tokenizer [FRESH_TOKENIZER]
-                        if passed create a new sentencepiece tokenizer model
+                        if passed create a new sentencepiece tokenizer model.
+                        Change args from file.
+  --corpus CORPUS       what will be the file to feed to tokenizer.
+```
+
+## Trainer File
+
+Once the parsing is completed we can then start to train / finetune GPT2 Model. If `finetune` option is chosen we use huggingface's `transformers.GPT2LMHeadModel`, else a new model is created based on your arguments. For arguments refer below:
+```
+➜  text2sql git:(master) ✗ python3 train_gpt2.py --help
+Train GPT2 model on t2sql corpus
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --tf {t,f}            Either to train the model from scratch (t) or finetune
+                        (f)
+  --pairs PAIRS         path to pairs dump
+  --tables TABLES       path to tables lm dump
+  --num_epochs NUM_EPOCHS
+                        Number of epochs to train / finetune
+  --save_folder SAVE_FOLDER
+                        Folder to save model to
+  --model MODEL         Saved model to have filepath `<model>.pt`
+  --save_every SAVE_EVERY
+                        Save model every this epoch
+  --tensorboard [TENSORBOARD]
+                        If passed, prepares tensorbaord summaries
+  --maxlen MAXLEN       Maximum sequence length
+  --n_embd N_EMBD       Embedding Dimension of model
+  --n_layer N_LAYER     Number of layers in the model
+  --n_head N_HEAD       Number of attention heads in the model
+  --activation_function ACTIVATION_FUNCTION
+                        Activation function to use in between the layers
+  --resid_pdrop RESID_PDROP
+                        Residual connection dropout probability
+  --embd_pdrop EMBD_PDROP
+                        Embedding connection dropout probability
+  --attn_pdrop ATTN_PDROP
+                        Attention connection dropout probability
+  --layer_norm_epsilon LAYER_NORM_EPSILON
+                        Layer norm epsilon value
+  --initializer_range INITIALIZER_RANGE
+                        Range for initializer
+  --summary_type SUMMARY_TYPE
+                        summary_type in GPTConfig
+  --summary_use_proj SUMMARY_USE_PROJ
+                        summary_use_proj in GPTConfig
+  --summary_activation SUMMARY_ACTIVATION
+                        summary_activation in GPTConfig
+  --summary_proj_to_labels SUMMARY_PROJ_TO_LABELS
+                        summary_proj_to_labels in GPTConfig
+  --summary_first_dropout SUMMARY_FIRST_DROPOUT
+                        summary_first_dropout in GPTConfig
+  --bos_token_id BOS_TOKEN_ID
+                        beggining of statement ID in vocabulary
+  --eos_token_id EOS_TOKEN_ID
+                        end of statement ID in vocabulary
 ```
 
 ## Datasets
 
-Using [CoSQL](https://yale-lily.github.io/cosql), [Spider](https://yale-lily.github.io/spider), [Sparc](https://yale-lily.github.io/sparc) datasets. Credit to the authors.
-
-There are a couple of things to note, we have in total 178 tables, but only 166 tables in training date and dev set has 20 tables.
+Using [CoSQL](https://yale-lily.github.io/cosql), [Spider](https://yale-lily.github.io/spider), [Sparc](https://yale-lily.github.io/sparc) datasets. Credit to the authors. There are a couple of things to note, we have in total 178 tables, but only 166 tables in training date and dev set has 20 tables.
---
+++ train_gpt2.py
@@ -0,0 +1,101 @@
+"""
+file to finetune / train from scratch the GPT2 model for Text2SQL dataset
+@yashbonde - 07.08.2020 (I love holidays)
+"""
+import os
+from argparse import ArgumentParser
+from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
+
+if __name__ == "__main__":
+    # load user args
+    args = ArgumentParser(description="Train GPT2 model on t2sql corpus")
+    args.add_argument("--tf", type=str, choices=["t", "f"],
+                      help="Either to train the model from scratch (t) or finetune (f)")
+
+    # data args
+    args.add_argument("--pairs", type=str, default="t2sql_pairs.tsv",
+                      help="path to pairs dump")
+    args.add_argument("--tables", type=str, default="t2sql_tables.tsv",
+                      help="path to tables lm dump")
+
+    # train args
+    args.add_argument("--num_epochs", type=int, default=20,
+                      help="Number of epochs to train / finetune")
+    args.add_argument("--save_folder", type=str,
+                      default="models", help="Folder to save model to")
+    args.add_argument("--model", type=str, default="t2sql_gpt",
+                      help="Saved model to have filepath `<model>.pt`")
+    args.add_argument("--save_every", type=int, default=1,
+                      help="Save model every this epoch")
+    args.add_argument("--tensorboard", nargs="?", default=True,
+                      help="If passed, prepares tensorbaord summaries")
+
+    # model config defaults set to one from GPT2Config
+    args.add_argument("--maxlen", default=1024, type=int,
+                      help="Maximum sequence length")
+    args.add_argument("--n_embd", default=768, type=int,
+                      help="Embedding Dimension of model")
+    args.add_argument("--n_layer", default=12, type=int,
+                      help="Number of layers in the model")
+    args.add_argument("--n_head", default=12, type=int,
+                      help="Number of attention heads in the model")
+    args.add_argument("--activation_function", default="gelu_new",
+                      type=str, help="Activation function to use in between the layers")
+    args.add_argument("--resid_pdrop", default=0.1, type=float,
+                      help="Residual connection dropout probability")
+    args.add_argument("--embd_pdrop", default=0.1, type=float,
+                      help="Embedding connection dropout probability")
+    args.add_argument("--attn_pdrop", default=0.1, type=float,
+                      help="Attention connection dropout probability")
+    args.add_argument("--layer_norm_epsilon", default=0.00001,
+                      type=float, help="Layer norm epsilon value")
+    args.add_argument("--initializer_range", default=0.02,
+                      type=float, help="Range for initializer")
+    args.add_argument("--summary_type", default="cls_index",
+                      type=str, help="summary_type in GPTConfig")
+    args.add_argument("--summary_use_proj", default=True,
+                      type=bool, help="summary_use_proj in GPTConfig")
+    args.add_argument("--summary_activation", default=None,
+                      type=bool, help="summary_activation in GPTConfig")
+    args.add_argument("--summary_proj_to_labels", default=True,
+                      type=bool, help="summary_proj_to_labels in GPTConfig")
+    args.add_argument("--summary_first_dropout", default=0.1,
+                      type=float, help="summary_first_dropout in GPTConfig")
+    args.add_argument("--bos_token_id", default=50256, type=int,
+                      help="beggining of statement ID in vocabulary")
+    args.add_argument("--eos_token_id", default=50256, type=int,
+                      help="end of statement ID in vocabulary")
+    args = args.parse_args()
+
+    # make dir if present
+    os.makedirs(args.save_folder, exist_ok=True)
+
+    MODE_TRAIN = {"t": True, "f": False}[args.tf]
+
+    if MODE_TRAIN:
+        print("🔋 Training the model from scratch")
+    else:
+        print("🔋 Finetuning model from huggingface's transformers")
+
+    # model config
+    config = GPT2Config(
+        vocab_size=args.vocab_size,
+        n_positions=args.maxlen,
+        n_ctx=args.maxlen,
+        n_embd=args.n_embd,
+        n_layer=args.n_layer,
+        n_head=args.n_head,
+        activation_function=args.activation_function,
+        resid_pdrop=args.resid_pdrop,
+        embd_pdrop=args.embd_pdrop,
+        attn_pdrop=args.attn_pdrop,
+        layer_norm_epsilon=args.layer_norm_epsilon,
+        initializer_range=args.initializer_range,
+        summary_type=args.summary_type,
+        summary_use_proj=args.summary_use_proj,
+        summary_activation=args.summary_activation,
+        summary_proj_to_labels=args.summary_proj_to_labels,
+        summary_first_dropout=args.summary_first_dropout,
+        bos_token_id=args.bos_token_id,
+        eos_token_id=args.eos_token_id,
+    )
